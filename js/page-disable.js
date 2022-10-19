705b391cc6b58640bd9daa08a1e195287c70f4cf
Alexandra Bubnova <alexandra.slob@gmail.com>
2022-10-19 23:36:16 +0300

    7.17. - переключения режимов страницы

---
+++ js/page-disable.js
@@ -0,0 +1,38 @@
+const offerForm = document.querySelector('.ad-form');
+const offerFormItems = offerForm.querySelectorAll('fieldset');
+const mapForm = document.querySelector('.map__filters');
+const mapFormSelects = mapForm.querySelectorAll('select');
+const mapFormFieldsets = mapForm.querySelectorAll('fieldset');
+
+const disableElements = (container) => {
+  container.forEach((item) => {
+    item.disabled = true;
+  });
+};
+const activateElements = (container) => {
+  container.forEach((item) => {
+    item.disabled = false;
+  });
+};
+
+
+const disablePage = () => {
+  offerForm.classList.add('ad-form--disabled');
+  mapForm.classList.add('ad-form--disabled');
+  disableElements(offerFormItems);
+  disableElements(mapFormSelects);
+  disableElements(mapFormFieldsets);
+
+};
+
+
+const activatePage = () => {
+  offerForm.classList.remove('ad-form--disabled');
+  mapForm.classList.remove('ad-form--disabled');
+  activateElements(offerFormItems);
+  activateElements(mapFormSelects);
+  activateElements(mapFormFieldsets);
+
+};
+
+export { disablePage, activatePage };
